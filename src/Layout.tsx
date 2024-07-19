1c430e24bc201908ae620563107d4cdbf10e251a
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-07-19 13:04:46 +0900

    refactor: Update react-router-dom to use HashRouter instead of BrowserRouter in Layout component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Content from './Content';
 
 function Layout() {
   return (
-    <Router basename='/portfolio'>
+    <Router>
       <div className="flex">
         <Sidebar />
         <main className="flex-1">
