84a23767cf10d299ff0b78a37d1e9c5997ca2497
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-07-20 18:25:59 +0900

    feature: Add typing animation to extend theme in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        typing: 'typing 2s steps(30), blink .3s step-end infinite alternate',
+      },
+      keyframes: {
+        typing: {
+          from: { width: '0' },
+          to: { width: '100%' }
+        },
+        blink: {
+          '50%': { borderColor: 'transparent' }
+        }
+      },
+    },
   },
   plugins: [],
 }
