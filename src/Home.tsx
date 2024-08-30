909a18ef197f7d30895e73ffb13d39f52a0320c9
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-08-31 00:21:15 +0900

    feat: change typing animation to typing svg

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,30 +1,6 @@
-import { useEffect, useState } from "react";
 import "tailwindcss/tailwind.css";
 
 function Home({ isSidebarOpen }: { isSidebarOpen: boolean }) {
-  const [text, setText] = useState("");
-  const [cursorVisible, setCursorVisible] = useState(true);
-  const fullText = "Hello! 👋 I'm muhi111";
-
-  useEffect(() => {
-    let index = 0;
-    const interval = setInterval(() => {
-      setText(fullText.slice(0, index));
-      index++;
-      if (index > fullText.length) clearInterval(interval);
-    }, 150);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setCursorVisible(false);
-    }, 4500); // 0.75s * 6 = 4.5s
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
     <div
       className={`flex items-center ${isSidebarOpen ? "justify-center" : "justify-center"} min-h-screen bg-gray-100 py-10`}
@@ -35,11 +11,10 @@ function Home({ isSidebarOpen }: { isSidebarOpen: boolean }) {
         <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <div className="p-8">
             <h1 className="text-4xl font-bold mb-6 text-gray-800">
-              <span
-                className={`inline-block ${cursorVisible ? "border-r-2 border-black animate-blink" : ""} overflow-hidden whitespace-nowrap animate-typing`}
-              >
-                {text}
-              </span>
+              <img
+                src="https://readme-typing-svg.demolab.com?font=Fira+Code&size=30&pause=1000&width=435&lines=Hi!+%F0%9F%91%8B++I'm+muhi111.+;I'm+a+student.;I'm+a+web+developer."
+                alt="Typing SVG"
+              />
             </h1>
             <div className="mb-6">
               <p className="text-xl mb-4 text-gray-700">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        typing: {
-          "0%": { width: "0ch" },
-          "100%": { width: "21ch" },
-        },
-        blink: {
-          "0%, 100%": { borderColor: "transparent" },
-          "50%": { borderColor: "black" },
-        },
-      },
-      animation: {
-        typing: "typing 4s steps(21, end), blink 0.75s steps(6, end)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
 };
