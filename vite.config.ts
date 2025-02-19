deaf3079ee564cf1c9e9286e79b2da5627024f93
muhi111 <107345594+muhi111@users.noreply.github.com>
2025-02-19 12:35:04 +0900

    fix: Integrate Tailwind CSS in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/portfolio/",
-	plugins: [react(), tsconfigPaths()],
+	plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
