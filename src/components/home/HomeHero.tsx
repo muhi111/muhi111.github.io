28db1d04767ee3d65c3199fb0582234dfea76ee8
muhi111 <107345594+muhi111@users.noreply.github.com>
2025-05-07 16:59:34 +0900

    feat: Update typing.svg font weight and text alignment in HomeHero component

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,13 +1,11 @@
 export default function HomeHero() {
 	return (
-		<div className="mb-6 md:mb-14">
-			<div className="relative w-full max-w-3xl mx-auto">
-				<img
-					src="/typing.svg"
-					alt="Hi!👋 I'm muhi111. I'm a student. And I'm a programmer."
-					className="w-full h-auto max-w-full"
-				/>
-			</div>
+		<div className="relative w-full max-w-3xl mx-auto">
+			<img
+				src="/typing.svg"
+				alt="Hi!👋 I'm muhi111. I'm a student. And I'm a programmer."
+				className="w-full h-auto max-w-full"
+			/>
 		</div>
 	);
 }
