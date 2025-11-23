ddaac422974fe4ae8e77d61f44985e20297f4035
muhi111 <107345594+muhi111@users.noreply.github.com>
2025-11-23 16:08:19 +0900

    Revert "feat: Viteのビルド設定にmanualChunksを追加し、ライブラリをグループ化"
    This reverts commit 17a729f2d293ffe374b98efaebd25a3e0740352d.

--- vite.config.ts
+++ vite.config.ts
@@ -6,31 +6,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	base: "/",
 	plugins: [react(), tsconfigPaths()],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes("node_modules")) {
-						if (
-							id.includes("@chakra-ui") ||
-							id.includes("@emotion") ||
-							id.includes("framer-motion") ||
-							id.includes("@zag-js") ||
-							id.includes("react-icons")
-						) {
-							return "ui";
-						}
-						if (
-							id.includes("react") ||
-							id.includes("scheduler") ||
-							id.includes("@remix-run")
-						) {
-							return "vendor";
-						}
-						return "utils"; // その他のライブラリをまとめる
-					}
-				},
-			},
-		},
-	},
 });
